d2eb9505a8411d3673c2972c6067ae2f615d5261
nguyenbeo <nguyenbeo@gmail.com>
2015-11-11 11:51:45 +0100

    first commit

---
+++ src/main/java/com/kthsoft/java8/java_8_new_features/App.java
@@ -0,0 +1,13 @@
+package com.kthsoft.java8.java_8_new_features;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
