73639c1228ce980af9b01a750ebd24f4275fd5b8
nguyenbeo <nguyenbeo@gmail.com>
2015-11-11 14:32:17 +0100

    Added tests for Optional and Lambdas

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # java-8-new-features
 Some tries to demonstrate new features of java 1.8
+* Optional class
+* Streams
+* Date / Time
+* Lambda
+* Nashorn JavaScript engine
+* Base64
+
+# Technology
+* Java 1.8
+* JUnit 4.12
---
+++ src/test/java/com/kthsoft/java8/OptionalTest.java
@@ -0,0 +1,51 @@
+package com.kthsoft.java8;
+
+import static org.junit.Assert.*;
+
+import java.util.Optional;
+
+import org.junit.Test;
+
+public class OptionalTest {
+
+	private static final String DEFAULT_STRING = "Default String";
+	private static final String NOT_NULL_STRING = "Not null string";
+	private static final String NULL_STRING = null;
+
+	@Test
+	public void testOptionalNull() {
+		Optional<String> testString = Optional.ofNullable(null);
+		
+		//Using isPresent of Optional to check null instead of object != null
+		assertFalse(testString.isPresent());
+	}
+	
+	@Test
+	public void testOptional() {
+		Optional<String> testStringOptional = Optional.of(NOT_NULL_STRING);
+		Optional<String> nullStringOptional = Optional.ofNullable(NULL_STRING);
+		
+		String testString = testStringOptional.orElse(DEFAULT_STRING);
+		assertTrue(testStringOptional.isPresent());
+		assertEquals(NOT_NULL_STRING, testString);
+		
+		String defaultString = nullStringOptional.orElse(DEFAULT_STRING);
+		assertEquals(DEFAULT_STRING, defaultString);
+		
+		// Using lambda () -> DEFAULT_STRING ~ Supplier<String>
+		String testOrElseGetString = nullStringOptional.orElseGet(() -> DEFAULT_STRING);
+		assertEquals(DEFAULT_STRING, testOrElseGetString);
+	}
+	
+	/**
+	 * Use map and filter with lambda
+	 */
+	@Test
+	public void testOptionalMore() {
+		Optional<String> testStringOptional = Optional.of(NOT_NULL_STRING);
+		
+		Optional<Person> mapStringOptional = testStringOptional.map(s -> new Person("Length is" + s.length()));
+		assertTrue(mapStringOptional.get().getName().equals("Length is" + NOT_NULL_STRING.length()));
+	}
+
+}
